57110e04b76a6595473fca1be248cf7b58809bb9
Nicola <modxit@modx.it>
2018-09-12 12:49:48 +0200

    commit

---
+++ install/setup.data.sql
@@ -0,0 +1,5 @@
+REPLACE INTO `{PREFIX}system_settings` 
+(setting_name, setting_value) VALUES 
+('rss_url_security','http://feeds.feedburner.com/evocms-security-news'),
+('rss_url_news', 'http://feeds.feedburner.com/evocms-release-news');
+
